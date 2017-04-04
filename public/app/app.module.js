0cb7ceecb205f2db2a095202bcd4a92a58b9e103
JamesTCS <james.lloyd@tcs.com>
2017-04-04 17:34:21 -0400

    Updated all 3rd party libraries

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -9,22 +9,24 @@
        'ui.bootstrap'
 
     ])
-    .config(['$routeProvider', function ($routeProvider) {
+    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
        $routeProvider
+         .when('/', {
+             templateUrl: 'app/pages/home/home.view.html',
+             controller: 'Home as vm'
+         })
+         .when('/another_page', {
+             templateUrl: 'app/pages/another_page/another_page.view.html',
+             controller: 'Another as vm'
+         })
+         .otherwise({
+             redirectTo: "/"
+         });
 
-           .when('/', {
-               templateUrl: 'app/pages/home/home.view.html',
-               controller: 'Home as vm'
-           })
-           .when('/another_page', {
-               templateUrl: 'app/pages/another_page/another_page.view.html',
-               controller: 'Another as vm'
-           })
-           .otherwise({
-               redirectTo: "/"
-           });
-       
+        $locationProvider.html5Mode(true);
+
+
 
     }]);
 
