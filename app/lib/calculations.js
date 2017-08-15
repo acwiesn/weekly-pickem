7a68cac7110ebd41ea209ab17cc3f335ba9ff979
Nestor A Hernandez <nestor.hernandez@tcs.com>
2017-08-15 13:55:42 -0400

    no message here

--- app/lib/calculations.js
+++ app/lib/calculations.js
@@ -1,28 +1,27 @@
 module.exports = {
 
     calculateSpread: function (game) {
-        console.log(game);
-        var score = parseInt(game.homeSpread,10)+parseInt(game.homeScore,10);
-        
-        console.log(score);
-        if(game.homeScore==null||game.awayScore==null){
+        if (game.homeScore == null || game.awayScore == null || game.homeScore == '' || game.awayScore == '') {
+            console.log('game has null some scores');
             return null;
-        }else if(score>game.awayScore){
+        }
+        var score = parseInt(game.homeSpread, 10) + parseInt(game.homeScore, 10);
+        console.log(score);
+        if (score > game.awayScore) {
             console.log('HomeTeam wins');
             return game.homeTeam;
-        }
-        else if(score==game.awayScore){
+        } else if (score == game.awayScore) {
             console.log('Game is pushed');
             return 'Push';
-        }
-        else if(score<game.awayScore){
+        } else if (score < game.awayScore) {
             console.log('AwayTeam wins');
             return game.awayTeam;
-        } 
+        } else{
+            throw new Error('Something bad happened on calculations');
+        }
     },
 
     checkWinner: function (y) {
-        return y-2;
+        return y - 2;
     }
 };
-
