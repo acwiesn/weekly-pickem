debce37aa40e5f8a95aa7f59a83e82d7b1f2ffe9
Nestor A Hernandez <nestor.hernandez@tcs.com>
2017-09-01 15:32:50 -0400

    asdf

--- app/lib/services.js
+++ app/lib/services.js
@@ -4,11 +4,23 @@ var Entry = require('../models/entries.js');
 var Schedule = require('../models/games.js');
 mongoose.set('debug', true);
 module.exports = {
-  getCurrentWeek: function(callback){
-      console.log('inside get current week number');
-      Schedule.find({current: true}, (err, data)=> {
-          console.log('inside find current week');
-          callback(err,data[0].week);
+    getCurrentWeek: function (callback) {
+        console.log('inside get current week number');
+        Schedule.find({
+            current: true
+        }, (err, data) => {
+            console.log('inside find current week');
+            callback(err, data[0].week);
         });
-  }
+    },
+    getWeeklyTotals: function (callback) {
+        Entry.find({}, (err, data) => {
+            var x = {};
+            for(var i = 0;<data.length;i++){
+                
+                x = {data[i].user:}
+            }
+            callback(err, x);
+        });
+    }
 };
